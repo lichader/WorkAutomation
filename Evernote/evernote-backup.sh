6b750a4402f868b249d5cbd0860ea6b6a130d872
Lichader <pengyu.7@gmail.com>
2019-03-09 10:27:57 +1100

    Minor change

--- Evernote/evernote-backup.sh
+++ Evernote/evernote-backup.sh
@@ -2,10 +2,18 @@
 echo "Start Exporting evernote notes"
 osascript "/Users/rpeng/Dropbox/Apps/WorkAutomation/Evernote/Export-All-Evernote-notes.applescript"
 echo "Export done! The file has been saved to Dropbox"
+
 echo "Start compressing file"
 cd /Users/rpeng/Dropbox/Apps/Evernote
-tar -zcvf "Evernote backup on $(date '+%Y-%m-%d').tar.gz" Evernote-notes.enex
+tar -zcvf "Backup_$(date '+%Y-%m-%d').tar.gz" Evernote-notes.enex 
+split -b 1024m "Backup_$(date '+%Y-%m-%d').tar.gz" "Backup_$(date '+%Y-%m-%d').tar.gz.part-"
+
+# uncompress
+# cat myfile_split.gz_* | gunzip -c > my_large_file
 echo "Finish compression"
+
 echo "Removing the original file"
 rm Evernote-notes.enex
+rm "Backup_$(date '+%Y-%m-%d').tar.gz"
+
 echo "Work complete"
